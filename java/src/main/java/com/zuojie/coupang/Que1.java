6bcabdffdf584e3574fc1683342c456a3d74552d
志枥 <zhili.zj@taobao.com>
2024-11-22 10:08:56 +0800

    coupang

---
+++ java/src/main/java/com/zuojie/coupang/Que1.java
@@ -0,0 +1,11 @@
+package com.zuojie.coupang;
+
+public class Que1 {
+    public void foo() {
+        System.out.println("abcd");
+    }
+    public static void main(String[] args) {
+        Que1 que1 = new Que1();
+        que1.foo();
+    }
+}
